c3c7aa3fe2480d9ff9ae3af0d34f541a106600aa
Marcinszopik <szopiks@yahoo.com>
2019-06-13 08:16:15 -0700

    Update scripts.js
    update scrips file so that the index page works correctly

--- js/scripts.js
+++ js/scripts.js
@@ -37,171 +37,17 @@ function toggle3() {
 	}
 }
 
-// Quiz Match Elements //
+// Quiz Match Elements --see js/scripts2.js //
 
 
 
-{
-var result;
 
 
 
 
-   function check()
-}
 
 
-   {
 
 
 
 
-      var question;
-
-      var value1;
-
-      var value2;
-
-      var value3;
-
-
-
-
-      question = 1;
-
-      value1 = Java Script;
-
-      value2 = Python;
-
-      value3 = CSharp;
-
-
-
-
-
-
-
-      result = "";
-
-
-
-
-      var choice;
-
-
-
-
-      for (question = 1; question <= 3; question++) {
-
-
-
-
-         var q = document.forms['quiz'].elements['q'+question];
-
-
-
-
-            for (var i = 0; i < q.length; i++) {
-
-               if (q[i].checked) {
-
-                  choice = q[i].value;
-
-               }
-
-            }
-
-
-
-
-            if (choice == "value1") {
-
-               value1++;
-
-            }
-
-
-
-
-            if (choice == "value2") {
-
-               value2++;
-
-            }
-
-
-
-
-            if (choice == "value3") {
-
-               value3++;
-
-
-
-
-            }
-
-
-
-
-         }
-
-
-
-
-      if (value1 >= value2 && value1 >= value3 && value1 > 0) {
-
-
-
-
-         // value1 is the highest
-
-         result = "Java Script";
-
-
-
-
-      }
-
-
-
-
-      else if (value2 > value1 && value2 >= value3 && value2 > 0) {
-
-
-
-
-         // value2 is the highest
-
-         result = "Python";
-
-
-
-
-      }
-
-
-
-
-      else if (value3 > value1 && value3 > value2 && value3 > 0) {
-
-
-
-
-         // value3 is the highest
-
-         result = "CSharp";
-
-
-
-
-
-
-
-
-
-
-
-
-
-   }
